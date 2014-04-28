17dc782ea3a6d516d2eefa84739e6bfa5aa50c18
Vladislava Tihomirova <tihomirova@yandex-team.ru>
2014-04-28 18:44:40 +0400

    fixes

---
+++ rebus.js
@@ -0,0 +1,10 @@
+function Animal (name) {
+    // TODO
+	this.name = name
+}
+
+new Animal("test").name; // test
+Animal("test").name;         // test
+
+//new Animal() instanceof Animal // true
+//Animal() instanceof Animal // true
